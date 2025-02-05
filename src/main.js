826cc20dcc1daefda6edc79453b0d9da2a474653
carson <carson@nerdware.ca>
2025-02-05 08:09:29 -0800

    Forgot a puzzle!! Also added some service worker stuff back in that I dont think is working
    Signed-off-by: carson <carson@nerdware.ca>

--- src/main.js
+++ src/main.js
@@ -5,6 +5,17 @@ import App from './App.vue'
 import { createPinia } from "pinia";
 import 'drag-drop-touch';
 
+if ('navigator' in window && window.navigator.standalone === undefined) {
+    const isIos = /iphone|ipad|ipod/.test(navigator.userAgent.toLowerCase());
+    if (isIos && !window.navigator.standalone) {
+        alert("Install this app: Tap the Share button, then 'Add to Home Screen'!");
+    }
+}
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/service-worker.js').then(() => {
+        console.log("Service Worker Registered");
+    });
+}
 const app = createApp(App);
 app.use(createPinia());
 app.mount('#app');
